b3015c699491683f20efa8302b38ae4686ec2286
yuying122 <yunming1005@gmail.com>
2023-11-10 01:45:32 +0800

    v0

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>資管二B 411146287 施艾岑</h1>"
+    homepage += "<a href=/MIS/personal-webpage>個人網站</a><br>"
+    return homepage
+
+
+@app.route("/MIS/personal-webpage")
+def course():
+    return render_template("aboutme.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
